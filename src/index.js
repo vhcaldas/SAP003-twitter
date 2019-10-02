6df0bd4ff7a85b1a4e8f83712631dfeeea0544da
vhcaldas <vivianhcaldas@gmail.com>
2019-10-02 19:07:12 -0300

    Second Version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+const maxLength = 140;
+
 const twitterText = document.querySelector("#twitter-text");
 const tweetButton = document.querySelector("#tweet-button");
-const tweetList = document.querySelector("#tweet-List")
+const tweetList = document.querySelector("#tweet-List");
+const twitterCounter = document.querySelector("#twitter-Counter");
+
 
 tweetButton.addEventListener("click", addTwitter);
 
@@ -9,4 +13,17 @@ function addTwitter(event) {
   newLi.textContent = twitterText.value;
   tweetList.appendChild(newLi);
   twitterText.value = "";
+}
+
+twitterText.addEventListener('keyup', changeCounter);
+
+function changeCounter(event) {
+  const counterValue = (maxLength - twitterText.value.length);
+  twitterCounter.textContent = counterValue;
+  if (counterValue ===maxLength) {
+    tweetButton.setAttribute('disabled', "");
+  } else {
+    tweetButton.removeAttribute('disabled');
+  }
+
 }
